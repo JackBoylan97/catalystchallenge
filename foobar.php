5bdbd60851dd892042fb4ac177b9ea34d30028f4
Jack Boylan <35166529+JackBoylan97@users.noreply.github.com>
2023-12-10 13:43:10 +1100

    adding switch statement alternative for foobar.php

--- foobar.php
+++ foobar.php
@@ -16,3 +16,31 @@ for($i = 1; $i <= 100; $i++){
 }
 echo PHP_EOL;
 
+//Can also be done with a switch statement
+
+/*
+ <?php
+for ($i = 1; $i <= 100; $i++) {
+    $output = '';
+
+    switch (true) {
+        case ($i % 3 === 0 && $i % 5 === 0):
+            $output = 'foobar';
+            break;
+        case ($i % 3 === 0):
+            $output = 'foo';
+            break;
+        case ($i % 5 === 0):
+            $output = 'bar';
+            break;
+        default:
+            $output = $i;
+    }
+
+    echo $output, ', ';
+}
+
+echo PHP_EOL;
+?>
+
+ */
